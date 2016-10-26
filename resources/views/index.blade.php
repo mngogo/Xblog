7cfa49851739a2271fa54d4218a62ca05f647fe9
lufficc <luffy.lcc@gmail.com>
2016-10-26 15:23:59 +0800

    UPDATE SOCIAL

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
         <p class="links">
             <font aria-hidden="true">»</font>
             <a href="{{ route('post.index') }}" aria-label="点击查看博客文章列表">博客</a><font aria-hidden="true">/</font>
-            <a href="{{ route('projects') }}" aria-label="点击查看项目列表">项目</a>
+            {{--<a href="{{ route('projects') }}" aria-label="点击查看项目列表">项目</a>--}}
             @foreach($pages as $page)
                 <font aria-hidden="true">/</font><a href="{{ route('page.show',$page->name) }}"
                                                     aria-label="查看{{ $author or 'author' }}的{{ $page->display_name }}">{{$page->display_name }}</a>
